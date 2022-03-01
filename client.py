6fbcc84a1eddab7502640caf3c94a87b36498e70
Chen Sasson <cheansa312@gmail.com>
2022-03-01 23:41:59 +0200

    fix: last before submission

--- client.py
+++ client.py
@@ -2,59 +2,54 @@ import pickle
 import socket
 import cv2
 import argparse
-from random import randint
+import cv2.cv2
+import keyboard
 
 HOST = '127.0.0.1'
 PORT = 65432
+BYTES_ORDER = "big"
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--display', default='random', help='display modes: loop, random')
-parser.add_argument('--time', default=5, help='int =time between display of each image')
-args = parser.parse_args()
 
-def open_image(img_path, delay_time):
+def menu():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--time', default='3000', help='int =time between display of each image in ms')
+    args = parser.parse_args()
+    return args
+
+
+def open_image(img_path, delay_time_action):
+    """
+    This function displays an image in a ceratain amount of time in ms.
+    :param img_path: the displayed image path
+    :param delay_time_action: action for cv2 display time
+    :return:
+    """
     image = cv2.imread(img_path)
-    image = cv2.resize(image, (500, 500))
-    cv2.imshow('image', image)
-    get_delay_time(delay_time)
-
-def get_delay_time(delay_time):
-    if int(delay_time) < 0:
-        return cv2.waitKey(0)
-    elif int(delay_time) >= 0:
-        return cv2.waitKey(delay_time)
-
-def get_random_image_path(list_images):
-    random_img_name = list_images[randint(0, len(list_images)-1)]
-    return random_img_name
+    image_resized = cv2.resize(image, (500, 500))
+    cv2.imshow('image', image_resized)
+    cv2.waitKey(delay_time_action)
 
 
-def get_loop_image_path(list_images,index):
-    if index < len(list_images)-1:
-        index += 1
-    else:
-        index = 0
-    return list_images[index], index
+def main():
+    args = menu()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        delay_time = args.time
+        print('To stop/close images display, press "q" key')
+
+        while True:
+            s.sendall(bytes(delay_time, "utf-8"))
+            list_image_display_data = s.recv(1048576)
+            img_path, index, delay_time_action = pickle.loads(list_image_display_data)
+            open_image(img_path, delay_time_action)
+            if keyboard.is_pressed('q'):
+                print('Pressed "q", image display terminated!')
+                break
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    display_mode = args.display
-    delay_time = args.time
-    index = 0
-
-    while True:
-        s.sendall(bytes(display_mode, "utf-8"))
-        data = s.recv(2048)
-        list_images = pickle.loads(data)
-
-        if display_mode == "random":
-            path = get_random_image_path(list_images)
-
-        elif display_mode == "loop":
-            path, new_index = get_loop_image_path(list_images, index)
-            index = new_index
-
-        open_image(path, delay_time)
-
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as err:
+        print(err)
 
